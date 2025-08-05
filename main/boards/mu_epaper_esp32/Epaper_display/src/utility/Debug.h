b52aaea2390e73fcb86eba469ee64a92b3d94a9e
juno-cyber <912783316@qq.com>
2025-08-05 23:44:22 +0800

    成功移植Epaper代码，后续开始改造

---
+++ main/boards/mu_epaper_esp32/Epaper_display/src/utility/Debug.h
@@ -0,0 +1,31 @@
+/*****************************************************************************
+* | File      	:	Debug.h
+* | Author      :   Waveshare team
+* | Function    :	debug with printf
+* | Info        :
+*   Image scanning
+*      Please use progressive scanning to generate images or fonts
+*----------------
+* |	This version:   V1.0
+* | Date        :   2018-01-11
+* | Info        :   Basic version
+*
+******************************************************************************/
+#ifndef __DEBUG_H
+#define __DEBUG_H
+
+// #include <Wire.h>
+#include "esp_log.h"
+
+#define USE_DEBUG 1
+#if USE_DEBUG
+	static const char *TAG = "EPAPER";  // 可改为你的模块名
+	#define Debug(__info) ESP_LOGI(TAG, "%s", __info)
+
+
+#else
+	#define Debug(__info)  
+#endif
+
+#endif
+
